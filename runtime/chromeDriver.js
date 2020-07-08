e0b4a6e422f4ef47f1017d4dc4447e211f66c586
Admin-Taft Wallace <ztwallace@favortech.local>
2020-07-08 14:58:24 -0400

    Added headless and no sandbox options for CI

--- runtime/chromeDriver.js
+++ runtime/chromeDriver.js
@@ -14,7 +14,7 @@ module.exports = function() {
         javascriptEnabled: true,
         acceptSslCerts: true,
         chromeOptions: {
-            args: ['start-maximized', 'disable-extensions']
+            args: ['--headless','--no-sandbox', 'disable-extensions']
         },
         path: chromedriver.path
     }).build();
